284130444a0ef5a4c548efa1193098601db94d72
Ryan Nett <rnett@calpoly.edu>
2018-06-09 14:59:35 -0700

    added table properties

--- Viewer/src/SolarSystem.java
+++ Viewer/src/SolarSystem.java
@@ -1,5 +1,8 @@
 //package exoplanetsolarsystemviewer; //comment out later when whole program gets integrated
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,9 +13,17 @@ import java.util.ArrayList;
 public class SolarSystem {
     private Star star;
 
-    private ArrayList<Planet> planets;
+    private final SimpleStringProperty name;
+    private final SimpleIntegerProperty planets;
+    private final SimpleIntegerProperty goldilocks;
+    private ArrayList<Planet> _planets;
 
     //TODO verify rs.previous() works as intended.
+    private int _golds = -1;
+
+    public Star getStar() {
+        return star;
+    }
 
     /**
      * Extracts the star and planets info from a join of planets and stars
@@ -22,49 +33,63 @@ public class SolarSystem {
      *  is projected. starName can never be null after natural joining.
      */
     public SolarSystem(ResultSet rs) throws SQLException {
-        
+
         boolean isFirstIteration = true; //used to get the star of a solar system
         boolean moreRows = false;
-        
+
         while ( (moreRows = rs.next()) ) {
-           
+
             if( isFirstIteration ) {
-                
+
                 star = new Star(rs);
 
-                planets = new ArrayList<Planet>();
-                
-                planets.add(new Planet(rs));
-                
+                _planets = new ArrayList<Planet>();
+
+                _planets.add(new Planet(rs));
+
                 isFirstIteration = false;
             } else {
-                
-                // no next or star changed.  
+
+                // no next or star changed.
                 //If star changed moreRows will be true, must revert back one row in preparation for next SolarSystem creation
                 // if starName is different, that implies current planet is not part of current solar system.
                 if (!moreRows || !rs.getString("starName").contentEquals(star.getStarName())) {
-                
+
                     break;
                 }
-            
 
-                planets.add(new Planet(rs));
-                
+
+                _planets.add(new Planet(rs));
+
                 }
-            
+
         }
 
         if (moreRows)
             rs.previous();
 
-    }
+        this.name = new SimpleStringProperty(getStar().getStarName());
+        this.planets = new SimpleIntegerProperty(getPlanets().size());
+        this.goldilocks = new SimpleIntegerProperty(getNumberGoldilocksPlanets());
 
-    public Star getStar() {
-        return star;
     }
 
     public ArrayList<Planet> getPlanets() {
-        return planets;
+        return _planets;
+    }
+
+    public int getNumberGoldilocksPlanets() {
+        if (_golds == -1) {
+            int c = 0;
+            for (Planet p : _planets) {
+                if (p.isGoldilocks())
+                    c++;
+            }
+
+            _golds = c;
+        }
+
+        return _golds;
     }
 
 }
